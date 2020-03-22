12e72152f803da4f32eb1b2fce99a44bbecbde0c
TommySoucy <soucy.tommy@gmail.com>
2020-03-22 13:28:32 -0400

    #23 Fixing comment tests

--- tests/Browser/CommentTest.php
+++ tests/Browser/CommentTest.php
@@ -47,8 +47,7 @@ class CommentTest extends DuskTestCase
 		
         $this->browse(function (Browser $browser) use ($user1) {
 			
-            $browser->click('navbarDropdown') // open user dropdown
-					->click('Logout') // log out user 0
+            $browser->visit('/logout')->logout() // log out user 0
 					->visit('/login') // go to login page
 					->type('email',$user1->email) // type in user email
 					->type('password','password') // type in user password
@@ -72,8 +71,7 @@ class CommentTest extends DuskTestCase
 		
         $this->browse(function (Browser $browser) use ($user0) {
 			
-            $browser->click('navbarDropdown') // open user dropdown
-					->click('Logout') // log out user 0
+            $browser->visit('/logout')->logout() // log out user 1
 					->visit('/login') // go to login page
 					->type('email',$user0->email) // type in user email
 					->type('password','password') // type in user password
